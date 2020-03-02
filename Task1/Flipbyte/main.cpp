0bb7a84c42cf0df1bbf308713709d9515e59c043
Daniil Zakharov <my@area.gq>
2020-03-02 12:02:44 +0300

    Put '{' on a newline

--- Task1/Flipbyte/main.cpp
+++ Task1/Flipbyte/main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <optional>
 
-bool IsArgumentAnIntegerNumber(char* arg) {
+bool IsArgumentAnIntegerNumber(char* arg)
+{
     const uint8_t MAX_ARG_LENGTH = 3; // 0 - 255 are the only valid values
 
     bool hasNonZeroMet = false;
     char* firstNonZeroIndex = nullptr; // To allow entering values like this: 000000000123
 
-    for (char* i = arg; *i != '\0'; i++) {
+    for (char* i = arg; *i != '\0'; i++)
+    {
         char ch = *i;
-        if (ch != '0' && !hasNonZeroMet) {
+        if (ch != '0' && !hasNonZeroMet)
+        {
             hasNonZeroMet = true;
             firstNonZeroIndex = i;
         }
-        if (!isdigit(ch)) {
+        if (!isdigit(ch))
+        {
             printf("Input value should only contain digits\n");
             return false;
         }
-        if (hasNonZeroMet && (i - firstNonZeroIndex >= MAX_ARG_LENGTH)) {
+        if (hasNonZeroMet && (i - firstNonZeroIndex >= MAX_ARG_LENGTH))
+        {
             printf("Input value is too big.\n");
             return false;
         }
@@ -25,18 +30,23 @@ bool IsArgumentAnIntegerNumber(char* arg) {
     return true;
 }
 
-std::optional<uint8_t> GetByte(char* byte) {
+std::optional<uint8_t> GetByte(char* byte)
+{
     int received = std::stoi(byte);
 
-    if (received < 0 || received > UINT8_MAX) {
+    if (received < 0 || received > UINT8_MAX)
+    {
         printf("Input value doesn't fit into a single byte\n");
         return std::nullopt;
-    } else {
+    }
+    else
+    {
         return received;
     }
 }
 
-uint8_t FlipBits(uint8_t originalByte) {
+uint8_t FlipBits(uint8_t originalByte)
+{
     uint8_t reversed = originalByte;
     reversed = (reversed & 0xF0) >> 4u | (reversed & 0x0F) << 4;
     reversed = (reversed & 0xCC) >> 2u | (reversed & 0x33) << 2;
@@ -44,24 +54,32 @@ uint8_t FlipBits(uint8_t originalByte) {
     return reversed;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         printf("Usage: flipbyte.exe <byte>\n");
         return 1;
     }
 
-    if (IsArgumentAnIntegerNumber(argv[1])) {
+    if (IsArgumentAnIntegerNumber(argv[1]))
+    {
         // nullopt if not in 0-255 range
         std::optional<uint8_t> byte = GetByte(argv[1]);
-        if (byte.has_value()) {
+        if (byte.has_value())
+        {
             uint8_t flipped = FlipBits(*byte);
 
             // Print value as integer
             std::cout << (unsigned int)flipped << std::endl;
-        } else {
+        }
+        else
+        {
             return 1;
         }
-    } else {
+    }
+    else
+    {
         return 1;
     }
 
