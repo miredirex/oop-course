abaad66ac96d2ba978c79f83137f878144edaae0
Daniil Zakharov <my@area.gq>
2020-03-02 12:51:08 +0300

    Fixes #2

--- Task1/Inverse/main.cpp
+++ Task1/Inverse/main.cpp
@@ -17,15 +17,30 @@ Matrix3x3 ReadMatrixFromStream(std::istream& stream)
     return mat;
 }
 
-void PrintMatrixToStream(std::ostream& stream, Matrix3x3& mat, char separator, unsigned int decimalPlaces)
+Matrix3x3 ReadMatrixFromFile(const char* fileName, int& errorCode)
 {
+    std::ifstream inputMatrixStream(fileName);
+    if (!inputMatrixStream.is_open() || inputMatrixStream.bad())
+    {
+        printf("Input file can not be opened or not found\n");
+        errorCode = 1;
+    }
+    return ReadMatrixFromStream(inputMatrixStream);
+}
+
+void PrintMatrixToStream(std::ostream& stream, const Matrix3x3& mat, char separator, unsigned int decimalPlaces)
+{
+    stream << std::setprecision(decimalPlaces) << std::fixed;
     for (unsigned int i = 0; i < Matrix3x3::MATRIX_SIZE; i++)
     {
         for (unsigned int j = 0; j < Matrix3x3::MATRIX_SIZE; j++)
         {
             double value = mat.GetValueAt(i, j) + 0.0; // To avoid -0.0 values
-            stream << round(value * pow(10.0, decimalPlaces)) / pow(10.0, decimalPlaces);
-            if (j < Matrix3x3::MATRIX_SIZE - 1) stream << separator;
+            stream << value;
+            if (j < Matrix3x3::MATRIX_SIZE - 1)
+            {
+                stream << separator;
+            }
         }
         stream << std::endl;
     }
@@ -39,14 +54,15 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    std::ifstream fileInputMatrix(argv[1]);
-    if (!fileInputMatrix.is_open() || fileInputMatrix.bad())
+    int errorCode = 0;
+    Matrix3x3 matrix = ReadMatrixFromFile(argv[1], errorCode);
+
+    if (errorCode)
     {
         printf("Input file can not be opened or not found\n");
         return 1;
     }
 
-    Matrix3x3 matrix = ReadMatrixFromStream(fileInputMatrix);
     std::optional<Matrix3x3> inverse = matrix.GetInverseMatrix();
 
     if (inverse.has_value())
